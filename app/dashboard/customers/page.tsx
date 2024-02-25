7643fdf76c9869f8c7bce9df7344775f1ef8a7f6
kinksn <kataoka0417@gmail.com>
2024-02-25 17:42:55 +0900

    Add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,31 @@
 import { Metadata } from "next";
+import { fetchCustomersPages } from "@/app/lib/data";
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import Pagination from '@/app/ui/invoices/pagination';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>
+export default async function Page({ searchParams }: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  }
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page || 1);
+  const totalPages = await fetchCustomersPages(query);
+  return (
+    <>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </>
+  );
 }
